361488a93e2c9c3674bcb4658314f216421fb9eb
Daniru2007 <daniruayuka@gmail.com>
2023-02-13 07:27:25 +0530

    simple updates on the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,21 +2,28 @@ static TT_ELEMENT: &str = "ELEMENT";
 static TT_CLASS_NAME: &str = "CLASS_NAME";
 static TT_ID_NAME: &str = "ID_NAME";
 static TT_TEXT: &str = "TEXT";
-static TT_NUMBER: &str = "NUMBER";
 static TT_ATTRIB_NAME: &str = "ATTRIB_NAME";
 static TT_LPARAN: &str = "LPARAN";
 static TT_RPARAN: &str = "RPARAN";
 static TT_LCURLY: &str = "LCURLY";
 static TT_RCURLY: &str = "RCURLY";
-static TT_DOT: &str = "DOT";
-static TT_HASH: &str = "HASH";
 static TT_NEWLINE: &str = "NEWLINE";
+static TT_EQ: &str = "EQ";
 static TT_EOF: &str = "EOF";
 
-static STRING_CHARS: [char; 56] = [
+static STRING_CHARS: [char; 78] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '_', '=', '~',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '_', '=', '~', '.',
+    ' ', ';', ':', '[', ']', '<', '>', '/', '?', '`', '!', '@', '#', '%', '^', '&', '*', '(', ')',
+    '"', '\\',
+];
+
+static TEXT_CHARS: [char; 65] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '_', '~', '1', '2',
+    '3', '4', '5', '6', '7', '8', '9', '0',
 ];
 
 fn is_element(element: &str) -> bool {
@@ -276,6 +283,10 @@ fn is_attrib(attrib: &str) -> bool {
     return attribs.contains(&attrib);
 }
 
+fn is_text(character: char) -> bool {
+    return TEXT_CHARS.contains(&character);
+}
+
 fn is_string(character: char) -> bool {
     return STRING_CHARS.contains(&character);
 }
@@ -324,20 +335,36 @@ impl Lexer {
 
     fn find_text(&mut self) -> String {
         let mut value = String::new();
-        while is_string(self.content_chars[self.index]) {
+        while is_text(self.content_chars[self.index]) {
             value.push(self.content_chars[self.index]);
             self.advance();
         }
         return value;
     }
 
-    fn find_str(&mut self) -> String {
+    fn find_string(&mut self) -> String {
         let mut value = String::new();
-        while self.content_chars[self.index] != '"' {
+        while is_string(self.content_chars[self.index]) {
+            if self.content_chars[self.index] == '"' {
+                self.advance();
+                break;
+            }
+            if self.content_chars[self.index] == '\\' {
+                self.advance();
+                match self.content_chars[self.index] {
+                    'n' => value.push('\n'),
+                    't' => value.push('\t'),
+                    '\\' => value.push('\\'),
+                    '\'' => value.push('\''),
+                    '"' => value.push('"'),
+                    _ => value.push(self.content_chars[self.index]),
+                };
+                self.advance();
+                continue;
+            }
             value.push(self.content_chars[self.index]);
             self.advance();
         }
-        self.advance();
         return value;
     }
 
@@ -347,39 +374,31 @@ impl Lexer {
             if character == '(' {
                 self.add_tok(TT_LPARAN, None);
                 self.advance();
-            }
-            if character == '(' {
-                self.add_tok(TT_LPARAN, None);
-                self.advance();
-            }
-            if character == ')' {
+            } else if character == ')' {
                 self.add_tok(TT_RPARAN, None);
                 self.advance();
-            }
-            if character == '{' {
+            } else if character == '{' {
                 self.add_tok(TT_LCURLY, None);
                 self.advance();
-            }
-            if character == '}' {
+            } else if character == '}' {
                 self.add_tok(TT_RCURLY, None);
                 self.advance();
-            }
-            if character == '.' {
+            } else if character == '.' {
                 self.advance();
                 let value = self.find_text();
                 self.add_tok(TT_CLASS_NAME, Some(&value[..]));
-            }
-            if character == '#' {
+            } else if character == '#' {
                 self.advance();
                 let value = self.find_text();
                 self.add_tok(TT_ID_NAME, Some(&value[..]));
-            }
-            if character == '"' {
+            } else if character == '=' {
                 self.advance();
-                let value = self.find_str();
+                self.add_tok(TT_EQ, None);
+            } else if character == '"' {
+                self.advance();
+                let value = self.find_string();
                 self.add_tok(TT_TEXT, Some(&value[..]));
-            }
-            if is_string(character) {
+            } else if is_text(character) {
                 let mut t_type = "";
                 let value = self.find_text();
                 if is_element(&value[..]) {
@@ -389,20 +408,18 @@ impl Lexer {
                 }
                 // assert_eq!(t_type, "");
                 self.add_tok(t_type, Some(&value[..]));
-            }
-            if character == ' ' {
+            } else if character == ' ' {
                 self.advance();
-            }
-            if character == '\t' {
+            } else if character == '\t' {
                 self.advance();
-            }
-            if character == '\n' {
+            } else if character == '\n' {
+                self.add_tok(TT_NEWLINE, None);
                 self.advance();
             } else {
-                println!("{}", character);
                 self.advance();
             }
         }
+        self.add_tok(TT_EOF, None);
         println!("[TOKENIZING]");
     }
 }
